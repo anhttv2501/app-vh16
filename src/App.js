dfca4ab060540aabbba51d5ec9f45a98b37e0caf
anhttv2501 <anhttv2501@gmail.com>
2026-03-23 17:18:59 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@
 function App() {
   return (
     <>
-    <h1>VH 16</h1>
+    <table border='1px'>
+      <thead>
+        <tr>
+          <th>STT</th>
+          <th>Ho va ten</th>
+          <th>Diem</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>To an An</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Bu thi Lua</td>
+          <td>8</td>
+        </tr>
+      </tbody>
+    </table>
     </>
   );
 }
